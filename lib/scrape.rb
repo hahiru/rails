d98fe8eaddedb248f8fbaea4891355a457ef315c
hahiru <tkhtisd@gmail.com>
2014-09-02 02:04:13 +0900

    add image

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -12,7 +12,7 @@ module Scrape
 
     url = target + "/" + thread + "/subback.html"
 
-    p url
+    p "Log :scrape url=" + url
 
     scrapeArray = []
 
@@ -26,13 +26,9 @@ module Scrape
 
     thread = doc.xpath('//small/a')
 
-    def printThread(url, title)
-      p url
-      p title
-    end
-
     thread.each do |item|
       firstUrl = target + item.attributes["href"].value
+      firstUrl = firstUrl.sub("l50","1")
       firstHtml = open(firstUrl) do |f|
         charset = f.charset
         f.read
@@ -44,9 +40,11 @@ module Scrape
         scrapeItem = {}
         threadBody.css('a').each do |aBody|
           if (aBody.attribute('href'))
-            if (aBody.attribute('href').value =~ /jpg/)
+            if (aBody.attribute('href').value =~ /jpg|png/)
+              p item.children.text
               scrapeItem["url"] = target + item.attributes["href"].value
               scrapeItem["title"] = item.children.text
+              scrapeItem["image"] = aBody.attribute('href').value
               scrapeArray.push(scrapeItem)
               break
             end
